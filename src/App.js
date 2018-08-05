f7c2cc595cd243b0e82b2092768a8ed8ca814fb7
Thomas Bouvier <tomatrocho@gmail.com>
2018-08-05 23:50:55 +0200

    Added custom colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import grey from '@material-ui/core/colors/grey';
 import PostIcon from '@material-ui/icons/Book';
 
 import AuthProvider from './AuthProvider';
@@ -9,11 +12,32 @@ import DataProvider from './DataProvider';
 import { PostShow, PostList, PostEdit, PostCreate } from './posts';
 import { EventShow, EventList, EventEdit, EventCreate } from './events';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: grey,
+    secondary: red,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Arial',
+      'sans-serif'
+    ].join(',')
+  }
+});
+
 const App = () => (
   <Admin
     authProvider={AuthProvider}
     dataProvider={DataProvider('https://insapp.insa-rennes.fr/api/v1')}
     title="Insapp"
+    theme={theme}
   >
     <Resource
       name="posts"
